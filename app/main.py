09c591724a4f148e559d922f51b0a193ab665fe4
kawal <kawaldhillon5@gmail.com>
2025-11-06 23:46:42 -0500

    added sqlite databse

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI, Request
-from typing import Optional
-from sqlmodel import Field, Session, SQLModel
+from fastapi import FastAPI
+from sqlmodel import create_engine, SQLModel
+from app.db.models.user_model import User
+from contextlib import asynccontextmanager
+
+data_base_URL = "sqlite:///./gym.db"
+engine = create_engine(data_base_URL, connect_args= {"check_same_thread": False})
+
+def create_db_and_tables():
+    print("Running create_db_and_tables")
+    SQLModel.metadata.create_all(engine)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    create_db_and_tables()
+    yield
+    # Shutdown
+    print("Shut Down")
 
 app = FastAPI(
-    title="Gym Tracker API"    
+    title="Gym Tracker API",
+    lifespan = lifespan    
 )
 
 @app.get('/')
