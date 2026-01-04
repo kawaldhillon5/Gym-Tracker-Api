9bdfbe17eb4fbe057f177eb882b795341508e339
kawal <kawaldhillon5@gmail.com>
2026-01-03 19:20:41 -0500

    added logic to work with postgreSQL

--- app/db/sqlite.py
+++ app/db/sqlite.py
@@ -4,10 +4,19 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+database_url = os.getenv("DATABASE_URL")
 
-data_base_URL = DATABASE_URL
-engine = create_engine(data_base_URL, connect_args= {"check_same_thread": False}) #type: ignore
+if database_url:
+
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    
+    engine = create_engine(database_url, echo=False)
+else:
+
+    sqlite_file_name = "gym.db"
+    sqlite_url = f"sqlite:///{sqlite_file_name}"
+    engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
